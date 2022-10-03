332e9072a8c3daca9b317276d7251075c848cf9b
BrunnoFrazao <bru_nno_18@hotmail.com>
2022-10-03 16:18:26 -0300

    aprendendo

---
+++ 006/exercicio.js
@@ -0,0 +1,7 @@
+var vel = 90
+var permitida = 80
+console.log(`A velocidade do seu carro e de ${vel}`)
+if(vel > 80){//condição simples
+    console.log('Você foi multado!')
+}
+console.log('Dirija sempre com cinto de segurança!')
---
+++ 006/exercicio2.js
@@ -0,0 +1,9 @@
+//condição composta
+var pais = 'Suriname'
+console.log(`Você está vivendo em ${pais}`)
+if(pais == 'Brasil'){
+    console.log('Você é Brasileiro!')
+}
+else{
+    console.log('Você é estrangeiro!')
+}
